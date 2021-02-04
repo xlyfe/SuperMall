87ee8f56b1f640a960a438f84a62be69162d9487
jason <xlyfe@163.com>
2021-02-04 09:07:06 +0800

    本周推荐和tabControl

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="tab-control">
+		<div class="tab-control-item" v-for="(item,index) in titles " :class="{active:index===currentIndex}" @click="itemClick(index)"
+		 :key="index">
+			<span>{{item}}</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'TabControl',
+		props: {
+			titles: {
+				type: Array,
+				default () {
+					return ['流行', '新款', '精选']
+				}
+			}
+		},
+		data() {
+			return {
+				currentIndex: 0
+			}
+		},
+		methods: {
+			itemClick(index) {
+				this.currentIndex = index
+			}
+		}
+	}
+</script>
+
+<style>
+	.tab-control {
+		display: flex;
+		text-align: center;
+		height: 44px;
+		line-height: 44px;
+		background-color: #fff;
+		box-shadow: 0 1px 1px rgba(100,100,100,.2);
+	}
+
+	.tab-control-item {
+		flex: 1;
+	}
+
+	.tab-control-item span {
+		padding: 5px;
+	}
+
+	.active {
+		color: var(--color-high-text);
+	}
+
+	.tab-control-item.active span {
+		border-bottom: 2px solid var(--color-high-text);
+	}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,12 +2,66 @@
 	<div id="home">
 		<nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
 		<home-swiper :banners="banners" />
+		<recommend-view :recommends="recommends" />
+		<feature-view />
+		<tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
+		<ul>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			</ul>
 	</div>
 </template>
 
 <script>
 	import NavBar from 'components/common/navbar/NavBar.vue'
 	import HomeSwiper from './childComps/HomeSwiper.vue'
+	import RecommendView from './childComps/RecommendView.vue'
+	import FeatureView from './childComps/FeatureView.vue'
+	
+	import TabControl from 'components/content/tabControl/TabControl.vue'
+	
 	import {		getHomeMulitdata	} from 'network/home.js'
 	
 	export default {
@@ -20,7 +74,10 @@
 		},
 		components: {
 			NavBar,
-			HomeSwiper
+			HomeSwiper,
+			RecommendView,
+			FeatureView,
+			TabControl
 		},
 		//生命周期函数创建
 		created() {
@@ -35,8 +92,20 @@
 </script>
 
 <style>
+	#home{
+		padding-top: 44px;
+	}
 	.home-nav {
 		background-color: var(--color-tint);
 		color: white;
+		position: fixed;
+		left: 0;
+		right: 0;
+		top: 0;
+		z-index: 9;
+	}
+	.tab-control{
+		position: sticky;
+		top: 44px;
 	}
 </style>
